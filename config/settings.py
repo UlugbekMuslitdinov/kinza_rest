4c4ddf229f5d8f36ae5e22c08b02c4b829c39110
Ulugbek <ulugbekmuslitdinov@gmail.com>
2021-08-26 22:52:06 +0500

    Static assests

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Third-party
     'rest_framework',
+    'crispy_forms',
 
     # Local
     'users.apps.UsersConfig',
@@ -133,7 +134,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
